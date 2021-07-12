94f838b444e3f90b1ccd417f41f709f2899e814e
Mattso <67868188+mattsolol@users.noreply.github.com>
2021-07-12 23:39:20 +0100

    basic Settings UI

--- Pangea/Views/SettingsView.swift
+++ Pangea/Views/SettingsView.swift
@@ -5,11 +5,99 @@
 //  Created by Luca on 12.07.21.
 //
 
+import Foundation
 import SwiftUI
 
 struct SettingsView: View {
     @Binding var isShowing: Bool
     var body: some View {
-        Text("Settings")
+        NavigationView {
+            VStack {
+                VStack(alignment: .center, spacing: 2) {
+                    Image(uiImage: UIImage(named: "pwnage") ?? UIImage())
+                        .resizable()
+                        .clipShape(Circle())
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .padding(8)
+            
+                    Text("RPwnage")
+                        .font(.system(size: 30))
+                        .bold()
+                    }
+            
+                VStack(spacing: 2) {
+                    Button {
+                        print("edit info")
+                    } label: {
+                        HStack {
+                            Image(systemName: "pencil")
+                                .foregroundColor(.white)
+                            Text("Edit my Information")
+                                .foregroundColor(.white)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .background(Color(.systemGray5))
+                    }
+                    
+                    Button {
+                        print("notifiction settings")
+                    } label: {
+                        HStack {
+                            Image(systemName: "exclamationmark.bubble.fill")
+                                .foregroundColor(.white)
+                            Text("Notifications")
+                                .foregroundColor(.white)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .background(Color(.systemGray5))
+                    }
+                    
+                    Button {
+                        print("privacy settings")
+                    } label: {
+                        HStack {
+                            Image(systemName: "eye.slash.fill")
+                                .foregroundColor(.white)
+                            Text("Privacy")
+                                .foregroundColor(.white)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .background(Color(.systemGray5))
+                    }
+                    
+                    Button {
+                        print("reset app")
+                    } label: {
+                        HStack {
+                            Image(systemName: "trash.fill")
+                                .foregroundColor(.white)
+                            Text("Reset App")
+                                .foregroundColor(.white)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .font(.system(size: 18))
+                        .padding()
+                        .background(Color.red)
+                    }
+                }
+                Spacer()
+            }
+            
+            .navigationTitle("Settings")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                Button {
+                    isShowing.toggle()
+                } label: {
+                    Text("Done")
+                }
+            })
+        }
     }
 }
