9cac80add0806239f643f9c8bd70a3c1bfd26c9f
Mattso <67868188+mattsolol@users.noreply.github.com>
2021-07-12 22:52:41 +0100

    Basic Contacts UI

--- Pangea/Views/ContactsView.swift
+++ Pangea/Views/ContactsView.swift
@@ -10,6 +10,36 @@ import SwiftUI
 struct ContactsView: View {
     @Binding var isShowing: Bool
     var body: some View {
-        Text("Contacts View")
+        NavigationView {
+            Form {
+                Section(header: Text("A")) {
+                    Text("Alan")
+                }
+                
+                Section(header: Text("J")) {
+                    Text("Jeff")
+                    Text("James")
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Contacts")
+            .toolbar(content: {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button {
+                        print("add")
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
+                    Spacer()
+                
+                    Button {
+                        isShowing.toggle()
+                    } label: {
+                        Text("Done")
+                    }
+                }
+            })
+        }
     }
 }
