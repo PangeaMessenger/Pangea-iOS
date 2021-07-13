b1a67538163d3638f5499f39dbb83980f6b2c217
Mattso <67868188+mattsolol@users.noreply.github.com>
2021-07-13 11:57:18 +0100

    created LoginView

---
+++ Pangea/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Pangea
+//
+//  Created by Mattso on 13/07/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationView {
+            Text("Login")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
